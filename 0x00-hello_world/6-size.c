683fbaf7f01d0365b6bf18ac3cd5fc59dfa439ae
kindness etim <kindnessjames.etim@gmail.com>
2022-12-09 19:29:08 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point to print sizes
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int a;
+	char c;
+	long int b;
+	long long int e;
+	float f;
+
+	printf("size of a char: %lu byte(s)\n", (unsigned long)(sizeof(c));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)(sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)(sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)(sizeof(e));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)(sizeof(f));
+	return (0);
+}

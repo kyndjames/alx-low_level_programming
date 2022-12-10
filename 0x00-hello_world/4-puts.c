83e71f440b62147e6b80a7405644e12b8f70d84c
kindness etim <kindnessjames.etim@gmail.com>
2022-12-09 17:33:18 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-/*
- * main - print a line of code using puts.
+/**
+ * main - Entry point print a line of code using puts
  *
- *Return: 0
-*/
-
+ * Return: 0
+ */
 int main(void)
-{ 
-
-	puts("\"Programming is like building a multilingual puzzle");
+{ puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

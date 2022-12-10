d4d22cf512a525ea02bf340719cb0dde435eb549
kindness etim <kindnessjames.etim@gmail.com>
2022-12-09 22:50:53 -0800

    hallo

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point to print sizes
- *
+ * code by kindness
  * Return: 0
  *
  */
@@ -10,13 +10,14 @@ int main(void)
 	char c;
 	int a;
 	long int b;
-	long long int e;
+	long long int d;
 	float f;
 
 	printf("size of a char: %lu byte(s)\n", (unsigned long)(sizeof(c));
 	printf("size of an int: %lu byte(s)\n", (unsigned long)(sizeof(a));
 	printf("size of a long int: %lu byte(s)\n", (unsigned long)(sizeof(b));
-	printf("size of a long long int: %lu byte(s)\n", (unsigned long)(sizeof(e));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)(sizeof)(d));
 	printf("size of a float: %lu byte(s)\n", (unsigned long)(sizeof(f));
 	return (0);
+
 }

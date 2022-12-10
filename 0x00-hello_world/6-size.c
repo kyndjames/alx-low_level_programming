a1d78b66f75900831c36a9ac4051f5bdff45decd
kindness etim <kindnessjames.etim@gmail.com>
2022-12-09 19:44:12 -0800

    owlh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,8 +15,8 @@ int main(void)
 
 	printf("size of a char: %lu byte(s)\n", (unsigned long)(sizeof(c));
 	printf("size of an int: %lu byte(s)\n", (unsigned long)(sizeof(a));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)(sizeof(b));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)(sizeof(e));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)(sizeof(f));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)(sizeof(b));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)(sizeof(e));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)(sizeof(f));
 	return (0);
 }

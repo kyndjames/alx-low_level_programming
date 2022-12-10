6ad3bf79fa89134647200806d0a8cadd1515277f
kindness etim <kindnessjames.etim@gmail.com>
2022-12-09 17:55:36 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point to print with printf
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%d", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

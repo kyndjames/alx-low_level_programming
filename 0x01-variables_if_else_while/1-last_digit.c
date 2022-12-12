3f86a30336650a92ec3198b7f979e4994726b300
kindness etim <kindnessjames.etim@gmail.com>
2022-12-11 21:51:01 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,11 +13,11 @@ int main(void)
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 lastDigit = n % 10;
-if (lastDigit < 5)
-printf("last digit of %d is %d and is less than 6 and not 0", n, lastDigit);
+if (lastDigit > 5)
+printf("last digit of %d is %d and is greater than 5 and not 0", n, lastDigit);
 else if (lastDigit == 0)
 printf("last digit of %d is %d and is 0", n, lastDigit);
 else
-printf("last digit of %d is %d and is greater than 5 and not 0", n, lastDigit);
-return 0;
+printf("last digit of %d is %d and is greater than 6 and not 0", n, lastDigit);
+return (0);
 }

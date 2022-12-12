6571a43e1f8c172073029d06c65ebc7d0663ee0d
kindness etim <kindnessjames.etim@gmail.com>
2022-12-12 01:16:22 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio>
+#include <ctype>
+/**
+ * main -Entry point
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (c <= 'z')
+	putchar(ch);
+	c++;
+	putchar('\n');
+	return (0);
+}

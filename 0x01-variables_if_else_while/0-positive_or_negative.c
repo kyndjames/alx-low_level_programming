d87654fa78ea06c6418c7b2994dddbf4d81b5142
kindness etim <kindnessjames.etim@gmail.com>
2022-12-11 18:33:36 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,9 +14,9 @@ int main(void)
 	if (n > 0)
 	printf("%d is positive", n);
 	else if (n == 0)
-	printf("%d is negative", n);
-	else
 	printf("%d is zero", n);
+	else
+	printf("%d is negative", n);
 	return (0);
 
 }

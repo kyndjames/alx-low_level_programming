727ff4de9c3ed17084716a9a58199a912f43c1b6
kindness etim <kindnessjames.etim@gmail.com>
2022-12-12 03:14:12 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int ch = 'a';
+
+	whlie(ch <= 'z');
+	{
+		if ((ch != 'e') && (ch != 'q'))
+		{
+		putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}

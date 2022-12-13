629f90f29f09753b29b8c9dcfea825c55fd1fb23
kindness etim <kindnessjames.etim@gmail.com>
2022-12-12 17:22:43 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ *  Return: 0
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	printf("%d\n", i);
+	return (0);
+}

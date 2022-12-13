73bbfdccaa68f3138459962fab807e84bd38aae8
kindness etim <kindnessjames.etim@gmail.com>
2022-12-12 19:33:45 -0800

    hfkfj

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *  Return: 0
+ *
+ */
+int main(void)
+{
+	int ones = '0';
+	int tens = '0';
+
+	for (ones = '0'; ones <= '9'; ones++)
+	{
+		for (tens = '0'; tens <= '9'; tens++)
+		{
+			if (!((ones == tens) || (tens > ones)))
+			{
+				putchar(ones);
+				putchar(tens);
+				if ((ones == 9 && tens == 8))
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

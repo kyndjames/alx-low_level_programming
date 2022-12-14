7c8303889e6b59f73a458a214f17e14e8f098cf1
kindness etim <kindnessjames.etim@gmail.com>
2022-12-14 06:47:34 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry piont
+ *
+ *  Return: 0
+ */
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+		_putchar('\n');
+		return (0);
+}

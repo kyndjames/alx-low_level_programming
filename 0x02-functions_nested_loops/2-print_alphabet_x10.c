aef8f76e87d8c73e18d77e2255655b7fdba21ed2
kindness etim <kindnessjames.etim@gmail.com>
2022-12-14 16:10:37 -0800

    gshsgd

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -> lowercase alphabet
+ */
+void print_alphabet_x10(void)
+{
+	int j;
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		-putchar(a);
+	}
+	}
+	_putchar('\n');
+}

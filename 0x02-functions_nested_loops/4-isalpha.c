8d32e3528c4921a5d6c89687df04b48793b10e07
kindness etim <kindnessjames.etim@gmail.com>
2022-12-15 11:56:01 -0800

    dfshsfddd

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isalpha ->  checks for alphabetic characte
+ * @c: a character on the aphabet
+ *
+ * Return: returns 0 or 1 depending on conditions
+ */
+int _isalpha(int c)
+{
+	return ((c => 'a' && c <= 'z') || (c => 'A' && c <= 'Z'));
+}

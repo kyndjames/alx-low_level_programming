925fd9a97489f9044b9b91e17b0992dc0eca2a1c
kindness etim <kindnessjames.etim@gmail.com>
2022-12-15 20:54:29 -0800

    djdhgfgf

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -26,11 +26,11 @@ void jack_bauer(void)
 				_putchar('0');
 				_putchar(j + '0');
 			}
-			else if (j > 10)
+				else if (j > 10)
 			{
-				 _putchar(':');
-				 _putchar((j / 10) + '0');
-				 _putchar((j % 10) + '0');
+				_putchar(':');
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
 			}
 		}
 	}

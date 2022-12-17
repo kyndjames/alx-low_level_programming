59b38c2a874ba8de592f81e606d3e01242fc9986
kindness etim <kindnessjames.etim@gmail.com>
2022-12-17 14:09:58 -0800

    aawa

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,15 +1,11 @@
 #include "main.h"
 /**
  * add - adds two integers and returns the result
- * @a: int to be added to b
- * @b: int to be added to a
- *  Return: sum of a and b
+ * @a: print int a
+ * @b: print int b
+ * *  Return: 0
  */
 int add(int a, int b)
 {
-	int c;
-
-	c = a + b;
-	
-	return (0);
+	return (a + b);
 }

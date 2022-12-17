b254a97f017b87fdab1e2dd2a89a78fbcc06acf8
kindness etim <kindnessjames.etim@gmail.com>
2022-12-17 13:31:34 -0800

    fhdegs

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,13 +8,13 @@ void times_table(void)
 	int a;
 	int b;
 	int c;
-	
+
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 9; b <= 9; b++)
 		{
 			c = a * b;
-			if ((c/10) == 0)
+			if ((c / 10) == 0)
 			{
 				if (b == 0)
 				{
@@ -23,7 +23,7 @@ void times_table(void)
 				if (b != 0)
 				{
 					_putchar(' ');
-					_putchar((c % 10)+ '0');
+					_putchar((c % 10) + '0');
 				}
 				if (b < 9)
 				{
@@ -32,18 +32,16 @@ void times_table(void)
 				}
 				else
 				{
-					_putchar((c / 10)+ '0');
-					_putchar((c % 10)+ '0');
-				}
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
 					if (b < 9)
 					{
 						_putchar(',');
 						_putchar(' ');
 					}
 				}
-		
 			}
 			_putchar('\n');
 		}
 	}
-
+}

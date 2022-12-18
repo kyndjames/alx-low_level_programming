97a7a58e9acc210b60629c40723ee8419f679927
kindness etim <kindnessjames.etim@gmail.com>
2022-12-18 06:24:00 -0800

    ahsfd

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - tests function that prints if integer is positive or negative
+ * main - tests function that prints positive or negative
  * Return: 0
  */
 int main(void)
@@ -9,6 +9,5 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }

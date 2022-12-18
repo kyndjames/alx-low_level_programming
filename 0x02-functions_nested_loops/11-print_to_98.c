f9e2b6d3e29e86b73c901e58076c03655ca1477d
kindness etim <kindnessjames.etim@gmail.com>
2022-12-17 20:09:47 -0800

    gfse

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * print_to_98- prints all natural numbers from n to 98
- * @n: number start counting from n to 98
+ * @n: number start counting from n to
  * Return: 0
  */
 void print_to_98(int n)

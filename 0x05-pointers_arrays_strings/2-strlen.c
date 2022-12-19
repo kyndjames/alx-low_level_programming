12f96fbd4564a2170e4cfb94416b8fda9b500ef2
kindness etim <kindnessjames.etim@gmail.com>
2022-12-19 14:24:21 -0800

    hdsgfd

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * int _strlen - returns the length of a string.
+ * @s: string
+ * Return: lenght
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	return (longi);
+}
+

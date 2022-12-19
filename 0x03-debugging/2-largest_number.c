5ad44a7fb954503d49a972df1bdcdffe1ebbf191
kindness etim <kindnessjames.etim@gmail.com>
2022-12-18 18:01:06 -0800

    jsgs

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,18 +20,20 @@ int largest_number(int a, int b, int c)
 
 {
 	int largest;
-	
-	if (a > b && a > c)
+
+	if (a > b)
 	{
-		largest = a;
-	}
-	else if (b > a && b> c)
-	{
-		largest = b;
+		if (b > c)
+			largest = a;
+		else if (a > c)
+			largest = a;
 	}
 	else
 	{
-		largest = c;
+		if (b < c)
+			largest = c;
+		else
+			largest = b;
 	}
 	return (largest);
 }

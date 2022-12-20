3fe07704bf325e478111277d5d2ea87378d39fb0
kindness etim <kindnessjames.etim@gmail.com>
2022-12-20 12:47:43 -0800

    kfh

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i = 0;
+	int j = 0;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+		_putchar(i);
+		i++;
+		}
+	_putchar('\n');
+	j++;
+	}
+}

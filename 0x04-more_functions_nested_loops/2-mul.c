1aa081b22b11b36820989c21ff24e4c2cfdffdb6
kindness etim <kindnessjames.etim@gmail.com>
2022-12-20 07:16:19 -0800

    xbsfs

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplication of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (int a * int b);
+}

90bb0ecc8c95099fdc57ae9b4603ee0d84941116
kindness etim <kindnessjames.etim@gmail.com>
2022-12-21 00:40:30 -0800

    hdgf

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Fizz-Buzz test
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int start = 1, end = 0;
+
+	while (start <= 100)
+	{
+		if (start % 3 == 0)
+			printf("Fizz");
+		if (start % 5 == 0)
+		printf("Buzz");
+		if (start % 3 != 0 && start % 5 != 0)
+			printf("%d", start);
+		putchar(start == end ? '\n' : ' ');
+		start++;
+	}
+	return (0);
+}

27a4deba1c1ba552949355170b10427550ceff39
kindness etim <kindnessjames.etim@gmail.com>
2022-12-30 09:59:12 -0800

    hchchvhv

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,27 +13,22 @@
  * Return: largest number
  *
  */
-
-
-
 int largest_number(int a, int b, int c)
 
 {
 	int largest;
 
-	if (a > b)
+	if (a > b && a > c)
 	{
-		if (b > c)
-			largest = a;
-		else if (a > c)
-			largest = a;
+		putchar(largest = a);
 	}
-	else
+	else if (b > a && b > c)
 	{
-		if (b < c)
-			largest = c;
-		else
-			largest = b;
+		putchar(largest = b);
+	}
+	else if (c > b && c > a)
+	{
+		putchar(largest = c);
 	}
 	return (largest);
 }

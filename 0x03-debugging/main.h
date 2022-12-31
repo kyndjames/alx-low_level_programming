04abc13a89a561b76909759968f8af56eb94c6d1
kindness etim <kindnessjames.etim@gmail.com>
2022-12-31 07:47:09 -0800

    gsfdds

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,5 +3,7 @@
 #define MAIN_H
 #include <stdio.h>
 int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
 void positive_or_negative(int i);
+int convert_day(int month, int day);
 #endif /* MAIN_H */

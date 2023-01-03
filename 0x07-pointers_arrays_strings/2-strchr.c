78399c4ea15dc2980ee60a524e29a1b053c9e35e
kindness etim <kindnessjames.etim@gmail.com>
2023-01-03 12:21:27 -0800

    ndm

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strchr -  locates a character in a string
+ * @s:  the string
+ * @c: the character 
+ * Return: c
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c && *s == '\0')
+			s++;
+		return (s);
+	}
+	return (NULL);
+}

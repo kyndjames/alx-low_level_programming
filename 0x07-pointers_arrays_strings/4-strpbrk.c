f2e0f51c92137a90b591a46cd1e61c31743cfa86
kindness etim <kindnessjames.etim@gmail.com>
2023-01-05 01:25:57 -0800

    dhsgd

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strpbrk - searchs a string for any of a set of bytes
+ * @s: the string to search
+ * @accept: the bytes to search for
+ * Return: a pointer to the byte in @s that matches one of the bytes
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}

72fd50bf9c06f397ab1b112491eefbc13f41303a
kindness etim <kindnessjames.etim@gmail.com>
2023-01-08 03:39:06 -0800

    gshsj

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in reverse.
+ * @s: reversed string
+ * Return: s
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_print_rev_recursion(s - 1);
+}

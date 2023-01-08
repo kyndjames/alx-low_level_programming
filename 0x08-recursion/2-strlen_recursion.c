0a12fce152518b74ff17505f03f748843fae2826
kindness etim <kindnessjames.etim@gmail.com>
2023-01-08 06:06:42 -0800

    ndfg

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,11 +2,13 @@
 /**
  * _strlen_recursion - returns the length of a string.
  * @s: string
+ * Return: lenght
  */
 int _strlen_recursion(char *s)
 {
 	int count;
 
-	for (count = 0; s[count] != '\0'; ++count);
+	for (count = 0; s[count] != '\0'; ++count)
 	return (count);
+	return (0);
 }

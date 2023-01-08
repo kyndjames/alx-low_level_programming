6582ce6f89c04cafd773fbcd1e65f4d853d2fbd1
kindness etim <kindnessjames.etim@gmail.com>
2023-01-08 14:14:02 -0800

    dssf

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * is_prime_number -  returns 1 if the input integer is a prime number
+ * @n: prime number
+ * Return: 1 if prime or else 0
+ */
+int is_prime_number(int n)
+{
+	if (n % 2 == 1 || n == 2)
+		return (1);
+	else
+		return (0);
+	return (is_prime_number(n + 1));
+}

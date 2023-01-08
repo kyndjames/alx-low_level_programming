eee910242688a580f2b0fe367ec007b1dd169855
kindness etim <kindnessjames.etim@gmail.com>
2023-01-08 05:03:38 -0800

    hdgfj

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,11 +7,10 @@
 void _print_rev_recursion(char *s)
 {
 	if (*s =='\0')
-	{
 		_putchar('\n');
-		return;
-	}
-	_putchar(*s);
-	++s;
+	return;
+	s++;
 	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }

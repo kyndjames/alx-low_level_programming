dd8561e7d7bcda124a1cd390e9da3677acc69d56
kindness etim <kindnessjames.etim@gmail.com>
2023-01-08 04:35:06 -0800

    jnmvn

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,11 +6,12 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s =='\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	_print_rev_recursion(s + 1);
 	_putchar(*s);
+	++s;
+	_print_rev_recursion(s);
 }

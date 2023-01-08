b085bb75658810f642efeeca5e515d1cb4edd8ad
kindness etim <kindnessjames.etim@gmail.com>
2023-01-08 09:43:28 -0800

    erts

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial - returns the factorial of a given number.
+ * @n: given number
+ * Return: integer
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
+}

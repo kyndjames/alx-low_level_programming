6c42d7938d25043c93cd593451efb5ed94d6baa5
kindness etim <kindnessjames.etim@gmail.com>
2023-01-08 13:23:34 -0800

    hsfdg

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+int _sqr_t(int n, int i);
+
+/**
+ * _sqrt_recursion -  returns the natural square root of a number.
+ * @n: numer to be sqrtd
+ * Retun: suareroot of n
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqr_t(n, 1));
+}
+
+/**
+ * _sqr_t - ffind square root of a number
+ * @n: the number to find the root
+ * @i: the iterator
+ * Return: squareroot
+ */
+int _sqr_t(int n, int i)
+{
+	int square = i * i;
+
+	if (square > n)
+		return (-1);
+	if (square == n)
+		return (i);
+	return (_sqr_t(n, i + 1));
+}

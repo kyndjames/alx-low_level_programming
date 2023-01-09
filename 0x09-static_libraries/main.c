93bc3d330a909ba7e22b6694f85ea427c76b3fb6
kindness etim <kindnessjames.etim@gmail.com>
2023-01-09 04:36:43 -0800

    gsrdte

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,6 +1,10 @@
 #include "main.h"
+/**
+ * main - the main function
+ * Return: 0
+ */
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+_puts("\"my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 	return (0);
 }

6ceeee522c061571482ac486560a8ee779ebb4a1
kindness etim <kindnessjames.etim@gmail.com>
2023-01-10 11:56:00 -0800

    sfgdh

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,19 +10,19 @@ char *_strdup(char *str)
 	char *pointr;
 	int str_ln = 0;
 	int count;
-
-	for (str_ln = 0; str[str_ln] != '\0'; ++str_ln)
+	
+	for (str_ln = 0; str[str_ln] != '\0'; str_ln++)
 	{
-		return (str);
+		str_ln++;
 	}
 	pointr = malloc(sizeof(char) * str_ln + 1);
 	if (pointr == NULL)
 	{
 		return (NULL);
 	}
-	for (count = 0; pointr[count] != '\0'; ++count)
+	for (count = 0; pointr[count] != '\0'; count++)
 	{
-		pointr[count] = str[str_ln];
+		pointr[count] = str[count];
 	}
 	return (pointr);
 }

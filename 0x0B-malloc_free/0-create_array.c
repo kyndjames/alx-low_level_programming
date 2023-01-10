5b06336d703735ac2b435a10eaf8c0b86ca568f8
kindness etim <kindnessjames.etim@gmail.com>
2023-01-10 03:56:43 -0800

    gdrefd

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars, and initializes it
+ * @size: size of the array
+ * @c: the character parameter
+ * Return: NULL if size = 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *arr;
+
+	arr = malloc(size * sizeof(char));
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+		i++;
+	}
+	return (arr);
+}

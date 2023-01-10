79f6296ad9db2d33ca7cc018edb4d77e9091d305
kindness etim <kindnessjames.etim@gmail.com>
2023-01-10 04:05:22 -0800

    bcbcgf

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 
 	arr = malloc(size * sizeof(char));
-	for (i = 0; i < size; i++)
+	for (i = 0; i <= size; i++)
 	{
 		arr[i] = c;
 		i++;

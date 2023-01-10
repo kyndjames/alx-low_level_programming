18abd0e31f5a0f74e4904255cfd9740e48b1260b
kindness etim <kindnessjames.etim@gmail.com>
2023-01-10 11:38:23 -0800

    kfjhg

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: string to be coppied
+ * Return: NULL if str = NULL
+ */
+char *_strdup(char *str)
+{
+	char *pointr;
+	int str_ln = 0;
+	int count;
+
+	for (str_ln = 0; str[str_ln] != '\0'; ++str_ln)
+	{
+		return (str);
+	}
+	pointr = malloc(sizeof(char) * str_ln + 1);
+	if (pointr == NULL)
+	{
+		return (NULL);
+	}
+	for (count = 0; pointr[count] != '\0'; ++count)
+	{
+		pointr[count] = str[str_ln];
+	}
+	return (pointr);
+}

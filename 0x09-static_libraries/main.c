a9b637b59291855b33397b11627cd059df9fc927
kindness etim <kindnessjames.etim@gmail.com>
2023-01-11 13:40:56 -0800

    gdhfg

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * main - the main function
- * Return: 0
+ * main - to like
+ * Return: voi
  */
 int main(void)
 {
-_puts("\"my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	_puts("\"At the end of the day, my goal hacker\"\n\t- Kevin Mitnick");
 	return (0);
 }

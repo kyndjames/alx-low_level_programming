0fc3b653188e6fe7e02014b53f80cc48556a3090
kindness etim <kindnessjames.etim@gmail.com>
2023-01-21 14:58:24 -0800

    mmfg

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_strings - function that prints strings
+ * @separator: string to be printed
+ * @n: numbers of strings passed to the function
+ * Return: 0
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int r;
+	char *str;
+	va_list ap;
+
+	va_start(ap, n);
+	for (r = 0; r < n; r++)
+	{
+		str = va_arg(ap, char *);
+		if (str)
+			printf("%s", str);
+		else
+			printf("nil");
+		if (r < n - 1)
+			if (separator)
+				printf("%s", separator);
+	}
+	va_end(ap);
+	printf("\n");
+}

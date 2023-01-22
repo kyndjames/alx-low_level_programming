c4d771e74f7f1b5592bfd5912ea745bff65e2d05
kindness etim <kindnessjames.etim@gmail.com>
2023-01-21 22:27:11 -0800

    mnbv

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -5,7 +5,7 @@
  * print_strings - function that prints strings
  * @separator: string to be printed
  * @n: numbers of strings passed to the function
- * Return: 0
+ * Return: no return
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
@@ -25,6 +25,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			if (separator)
 				printf("%s", separator);
 	}
-	printf("\n");
 	va_end(ap);
+	printf("\n");
 }

1922fb23fa85f7d2a555f9c919197a7a392467f9
kindness etim <kindnessjames.etim@gmail.com>
2023-01-21 22:52:37 -0800

    hgft

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (str)
 			printf("%s", str);
 		else
-			printf("nil");
+			printf("(nil)");
 		if (r < n - 1)
 			if (separator)
 				printf("%s", separator);

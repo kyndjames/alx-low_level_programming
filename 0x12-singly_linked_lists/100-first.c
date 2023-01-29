c7a375fbc8751a2ab1bcde834e1ac95af4b868f7
kindness etim <kindnessjames.etim@gmail.com>
2023-01-29 14:37:23 -0800

    jshgd

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void myfunct(void);
+/**
+ * myfunct - prints a sentence
+ * function is excuted
+ */
+void myfunct(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

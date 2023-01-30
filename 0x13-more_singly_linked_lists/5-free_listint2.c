7723efd0c3fda9c1c1fa17ca7589d3daa91e43ca
kindness etim <kindnessjames.etim@gmail.com>
2023-01-30 08:16:18 -0800

    gfhsf

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - function that frees a listint_t list.
+ * @head: pointer to the new node
+ * Return: The function sets the head to NULL
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr1, *ptr2 = *head;
+
+	while (ptr2)
+	{
+		ptr1 = ptr2->next;
+		free(ptr2);
+		ptr2 = ptr1;
+	}
+	*head = NULL;
+}

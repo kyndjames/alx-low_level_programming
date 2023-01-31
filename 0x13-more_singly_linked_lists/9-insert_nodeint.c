2712b1573251fc72f41c429275c9a116753204ac
kindness etim <kindnessjames.etim@gmail.com>
2023-01-30 21:44:58 -0800

    gdbv

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 		ptr2 = ptr2->next;
 	}
-	ptr2->next = ptr2->next;
+	ptr1->next = ptr2->next;
 	ptr2->next = ptr1;
 	return (ptr1);
 }

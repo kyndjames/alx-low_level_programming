11ae10f9df935406b5e8e44dce7d00991794f179
kindness etim <kindnessjames.etim@gmail.com>
2023-01-31 02:14:59 -0800

    fdgd

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,4 +1,6 @@
 char *create_array(unsigned int size, char c);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 int _putchar(char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
